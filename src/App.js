2d861e1e24fd829733bf0a536e045119481cc77c
bitnick20 <65895288+bitnick20@users.noreply.github.com>
2020-08-06 18:24:45 +0200

    add react_trippy

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import HomeContainer from './containers/HomeContainer';
+
+
+class Apps extends React.Component {
+  render () {
+    return (
+      <Router>
+        <div>
+        <Switch>
+          <Route path="/">
+            <HomeContainer />
+          </Route>
+        </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default Apps;
